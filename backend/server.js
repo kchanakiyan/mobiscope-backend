1bb010e9e641dca4594a5c73b26f6054a559a45a
kchanakiyan <chanakiyank.cse2024@citchennai.net>
2026-08-08 00:32:54 +0530

    Initial MOBISCOPE Backend

---
+++ backend/server.js
@@ -0,0 +1,87 @@
+const express = require("express");
+const pool = require("./db");
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = 3000;
+
+// Home Route
+app.get("/", (req, res) => {
+  res.send("🚍 MOBISCOPE Backend Running...");
+});
+
+// Database Test Route
+app.get("/students", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM students");
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Database Error");
+  }
+});
+
+// Attendance API
+// Attendance API
+app.post("/api/attendance", async (req, res) => {
+
+    try {
+
+        const { rfid_uid, bus_id } = req.body;
+
+        // Find student using RFID
+        const student = await pool.query(
+            "SELECT * FROM students WHERE rfid_uid = $1",
+            [rfid_uid]
+        );
+
+        if (student.rows.length === 0) {
+            return res.status(404).json({
+                success: false,
+                message: "RFID not found"
+            });
+        }
+
+        const s = student.rows[0];
+
+        // Insert attendance
+        await pool.query(
+            `INSERT INTO attendance (student_id, bus_id)
+             VALUES ($1,$2)`,
+            [s.student_id, bus_id]
+        );
+
+        res.json({
+            success: true,
+            student_name: s.student_name,
+            parent_name: s.parent_name,
+            parent_phone: s.parent_phone,
+            message: "Attendance marked successfully"
+        });
+
+    } catch (err) {
+        console.log(err);
+
+        res.status(500).json({
+            success:false,
+            message:"Server Error"
+        });
+    }
+
+});
+
+// GPS API
+app.post("/api/location", (req, res) => {
+  console.log(req.body);
+
+  res.json({
+    success: true,
+    message: "Location updated successfully"
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server Running on http://localhost:${PORT}`);
+});
